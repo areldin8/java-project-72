47ad1610122ee9b12d032531dc24178d943c8246
areldin8 <belphegor26ra@gmail.com>
2024-11-03 00:58:35 +0300

    Hello World

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,19 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
+    application
+    checkstyle
+    jacoco
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -12,8 +26,31 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("org.slf4j:jcl-over-slf4j:2.0.16")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.start(7070);
+    }
+}
+
