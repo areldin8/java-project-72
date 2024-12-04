8ac37968f71b08bd351a2402f35f6065f476a909
areldin8 <belphegor26ra@gmail.com>
2024-12-04 18:17:05 +0300

    front-end ch2

--- app/src/main/java/hexlet/code/util/Utils.java
+++ app/src/main/java/hexlet/code/util/Utils.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Utils {
     public static String formatDate(LocalDateTime date) {
-        return date != null ? date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) : "";
+        return date != null ? date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) : "";
     }
 }
 
